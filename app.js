790fafee9d8da174584e644834d60595444a9c9c
meetpatel2908 <meetjpatel1234@gmail.com>
2025-07-19 22:16:46 +0530

    Add start script for Railway

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/students', studentRoutes);
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
